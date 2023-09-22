b97eb1d92a3ad85d7d32de9ed618bb2efb9c4290
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-09-22 00:16:40 -0600

    Got Regex working properly for Bukkit-style logs

--- mc-log-aggregator.go
+++ mc-log-aggregator.go
@@ -1,31 +1,31 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
+	"fmt"
 	"net/http"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
-// // urlExample := "postgres://username:password@localhost:5432/database_name"
-// conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-// if err != nil {
-// 	fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-// 	os.Exit(1)
-// }
-// defer conn.Close(context.Background())
+// -------------- Global Variables --------------
 
-// var name string
-// var weight int64
-// err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
-// if err != nil {
-// 	fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-// 	os.Exit(1)
-// }
-
-// fmt.Println(name, weight)
+var postgresConn *pgx.Conn
 
 func main() {
+	var err error
+	postgresConn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer postgresConn.Close(context.Background())
+
 	// Get IP from env
 	ip := os.Getenv("IP_ADDRESS")
 	if ip == "" {
@@ -47,14 +47,130 @@ func main() {
 	router.GET("/test", getTest)
 	router.POST("/upload", uploadTest)
 
-	router.Run(ip + ":" + port)
+	// router.Run(ip + ":" + port)
+
+	stringList := parseFile("./uploads/2022-04-20-2.log")
+
+	uploadToDatabase("Hub", "Bukkit", "test0101", stringList)
 }
 
 // -------------- Structs --------------
 
+type LogEntry struct {
+	Server     string
+	ServerType string
+	Timestamp  string
+	Level      string
+	Source     string
+	Message    string
+}
+
 // -------------- Enums --------------
 
 // -------------- Functions --------------
+
+// Function to parse a base64 encoded string blob and split it into an array of strings
+func parseBlob(blob string) []string {
+	text, err := base64.StdEncoding.DecodeString(blob)
+	if err != nil {
+		fmt.Println("decode error:", err)
+		return nil
+	}
+
+	stringList := strings.Split(string(text), "\n")
+
+	// for _, str := range stringList {
+	// 	fmt.Println(str)
+	// }
+
+	return stringList
+}
+
+// Function to parse a file and split it into an array of strings
+func parseFile(file string) []string {
+	// Read the file
+	data, err := os.ReadFile(file)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	// Convert the file to a string
+	stringData := string(data)
+
+	// Split the string into an array of strings
+	stringList := strings.Split(stringData, "\n")
+
+	// for _, str := range stringList {
+	// 	fmt.Println(str)
+	// }
+
+	return stringList
+}
+
+// Function to parse an array of strings and upload them to the database
+func uploadToDatabase(server string, serverType string, logDate string, stringList []string) {
+	// Timestamp in the form of "[02:26:00]"
+	timeStampRegex := regexp.MustCompile(`\[\d{2}:\d{2}:\d{2}\]`)
+	// Level in the form of "[text]:"
+	levelRegex := regexp.MustCompile(`\[([^\]]+)\]:`)
+	// Source in the form of ": [text]"
+	sourceRegex := regexp.MustCompile(`: \[([^\]]+)\]`)
+	// Message in the form of "] The text is yes"
+	// messageRegex := regexp.MustCompile(`\] .+`)
+
+	var logEntries []LogEntry
+
+	for _, str := range stringList {
+		// Check for a timestamp
+		if timeStampRegex.MatchString(str) {
+			// Get the timestamp
+			timestamp := timeStampRegex.FindString(str)
+			// Get the level
+			level := levelRegex.FindString(str)
+			// Get the message
+			message := str
+			// Get the source
+			source := sourceRegex.FindString(message)
+			// Remove the : from the source
+			source = strings.Trim(source, ":")
+
+			// Remove everything from the message except the message
+			message = strings.Replace(message, level+" ", "", 1)
+			message = strings.Replace(message, source, "", 1)
+			message = strings.Replace(message, timestamp+" ", "", 1)
+
+			// Remove the brackets from the timestamp
+			timestamp = strings.Trim(timestamp, "[]")
+			// Remove the brackets from the level
+			level = strings.Trim(level, "[]:")
+			// Remove the brackets from the source
+			source = strings.Trim(source, " []")
+
+			// Create a log entry
+			logEntry := LogEntry{
+				Server:     server,
+				ServerType: serverType,
+				Timestamp:  logDate + " " + timestamp,
+				Level:      level,
+				Source:     source,
+				Message:    message,
+			}
+
+			// Append the log entry to the log entries
+			logEntries = append(logEntries, logEntry)
+		} else {
+			// Assume that the message is a continuation of the previous message
+			// Append the message to the previous message
+			logEntries[len(logEntries)-1].Message += str
+		}
+
+		for _, logEntry := range logEntries {
+			fmt.Println("--------------------")
+			fmt.Println(logEntry)
+		}
+	}
+}
 
 // -------------- Handlers --------------
 
--- schema/postgresql.sql
+++ schema/postgresql.sql
@@ -2,7 +2,9 @@ CREATE TABLE "mc_logs" {
     pkey UUID NOT NULL DEFAULT gen_random_uuid() , 
     CONSTRAINT pkey_mc_logs PRIMARY KEY ( pkey ) ,
     "server" VARCHAR(50) NOT NULL ,
+    "server_type" VARCHAR(50) NOT NULL ,
     "timestamp" TIMESTAMP NOT NULL DEFAULT now() ,
     "level" VARCHAR(10) NOT NULL ,
+    "source" VARCHAR(50) NOT NULL ,
     "message" TEXT NOT NULL
 }
